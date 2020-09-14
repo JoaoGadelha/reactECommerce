92a7bb6cc38bfb7c3841a47c05301f09dae496aa
Joao <joaoricardotg@gmail.com>
2020-09-13 22:32:13 -0300

    Carousel 2
    ** Finished implementing tablet version of carousel 2

---
+++ src/ComponentsMobile/HomePage/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+
+    return (
+        <div className='footer-mobile'>
+            <div className='container'>
+                <h5>
+                    Telesales
+                </h5>
+                <span>Capitals and metropolitan regions</span>
+                <button>
+                    Call 2003-4773
+                </button>
+                <h5>
+                    Other regions:<span> (266) 855-0710</span>
+                </h5>
+                <h5><img src={'https://m.casasbahia.com.br/assets/images/ic-new-seta-direita.svg'}></img>Customer service</h5>
+                <h5><img src={'https://m.casasbahia.com.br/assets/images/ic-new-seta-direita.svg'}></img>Most search brands and products</h5>
+            </div>
+        </div>
+    )
+
+}
+
+export default Footer
