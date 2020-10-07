6dd384354b7ca97ae79ab2d603e4f2027e952429
Joao <joaoricardotg@gmail.com>
2020-10-07 20:05:49 -0300

    updating Shopping Cart

--- src/Components/ShopCartPage/CartItem.js
+++ src/Components/ShopCartPage/CartItem.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 const CartItem = (props) => {
-let product = props.product;
-return (
-    <div>
+    let product = props.product;
+    let index = props.index;
+    let array = props.array;
+    console.log(product);
+    return (
+        <div>
+            <img src={product.image}></img>
+            <h1>{product.quantity}</h1>
+            <h1>{product.price}</h1>
+            <h1>{product.price*product.quantity}</h1>
+            <button onClick={()=>{
 
-    </div>
-)
+            }}>Remove</button>
+        </div>
+    )
 }
 
 export default CartItem
--- src/Components/ShopCartPage/ShopCartPage.js
+++ src/Components/ShopCartPage/ShopCartPage.js
@@ -9,14 +9,9 @@ import CartItem from './CartItem'
 const ShopCartPage = () => {
     let { userId, shopCart, setShopCart } = useContext(Context);
     let reqBody = { userId: userId };
-    let [prodList, setProdList] = useState([]);
-    let prodListAux = [];
-    // let [firstRender1, setFirstRender1] = useState(true);
-    // let [firstRender2, setFirstRender2] = useState(true);
-    // let [firstRender3, setFirstRender3] = useState(true);
-    let [auxState, setAuxState] = useState('');
     let isFirstRender = useIsFirstRender();
-    let [fin1, setFin1] = useState(false); // finished function 1
+    let [runFlag, setRunFlag] = useState(false);
+    let [count, setCount] = useState(1);
 
     // fetches the id's of all products in the user's shopping cart
     useEffect(() => {
@@ -25,49 +20,63 @@ const ShopCartPage = () => {
                 let auxCart = [];
                 for (let i = 0; i < data.shopCart.length; i++) {
                     auxCart[i] = {};
-                    auxCart[i].id = data.shopCart[i];
+                    auxCart[i].id = data.shopCart[i].id;
+                    auxCart[i].quantity = data.shopCart[i].quantity;
                 }
                 setShopCart(auxCart); // JSON data parsed by `data.json()` call
             });
     }, [])
 
     useEffect(() => {
-        console.log(isFirstRender);
-        if (!isFirstRender &&
-            shopCart[0].image === undefined &&
-            shopCart[0].id !== undefined) {
+        if (!isFirstRender && shopCart[0] !== undefined && Object.keys(shopCart[0]).length === 2 && runFlag === false)
+            setRunFlag(true);
+    }, [shopCart])
+
+    useEffect(() => {
+        console.log('cart');
+        console.log(shopCart.length);
+        if (!isFirstRender && runFlag) {
             for (let i = 0; i < shopCart.length; i++) {
                 fetch(`https://api-do-joao.herokuapp.com/find/id/${shopCart[i].id}`).
-                then(res => res.json()).
-                then(res => {
-                    let aux = shopCart;
-                    aux[i].image = res[0].image;
-                    aux[i].title = res[0].title;
-                    aux[i].price = res[0].price;
-                    aux[i].oldPrice = res[0].oldPrice;
-                    aux[i].type = res[0].type;
-                    setShopCart(aux);
-                    console.log(shopCart);
-                });
+                    then(res => res.json()).
+                    then(res => {
+                        let aux = shopCart;
+                        console.log(aux[i], res[0]);
+                        aux[i].image = res[0].image;
+                        aux[i].title = res[0].title;
+                        aux[i].price = res[0].price;
+                        aux[i].oldPrice = res[0].oldPrice;
+                        aux[i].type = res[0].type;
+                        setShopCart(aux);
+                    });
             }
         }
-    }, [shopCart]);
+    }, [runFlag]);
 
-    
+    useEffect(() => {
+        if(count <= 2001){
+        setCount(prev => prev + 1);
+        }
+        console.log(count);
+    }, [count])
 
-
-    if (shopCart !== undefined && Object.keys(shopCart).length > 0 && auxState !== '') {
+    if (count >= 2000) {
+        console.log(shopCart);
         return (
             <div class={styles.container}>
                 <h1>Product</h1><h1>Quantity</h1><h1>Unit price</h1><h1>Total price</h1><div></div>
-            {
-                shopCart.map(product => <CartItem key={product.id} product={product}/>)
-            }
+                {
+                    shopCart.map((product, i, array)   => <CartItem key={product.id} product={product} index={i} array={array}/>)
+                }
             </div>
         )
     } else {
+
         return (
-            <div>Empty</div>
+
+            <div><button onClick={() => {
+                console.log(shopCart[0].price);
+            }}>Show</button></div>
         )
     }
 }
