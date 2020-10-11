11374ebb9cb33d580f58d3de508d0d78309c5859
Joao <joaoricardotg@gmail.com>
2020-10-11 18:06:25 -0300

    Finished implementing the logic of the shopping cart page

--- src/Components/ShopCartPage/ShopCartPage.js
+++ src/Components/ShopCartPage/ShopCartPage.js
@@ -4,74 +4,42 @@ import { Context } from '../../Context'
 import { postData, useIsFirstRender } from '../Common/CommonFunctions'
 import CartItem from './CartItem/CartItem'
 
-
 const ShopCartPage = () => {
     let { userId, shopCart, setShopCart } = useContext(Context);
-    let isFirstRender = useIsFirstRender();
-    let [runFlag, setRunFlag] = useState(false);
-    let [count, setCount] = useState(0);
 
-    // fetches the id's of all products in the user's shopping cart
-    useEffect(() => {
-        console.log('useEffect1');
-        let auxCart = [];
-        postData(`https://electroshopping-user-regist.herokuapp.com/getShopCart/${userId}`, { userId: userId })
-            .then(data => {
-                for (let i = 0; i < data.shopCart.length; i++) {
-                    auxCart[i] = {};
-                    auxCart[i].id = data.shopCart[i].id;
-                    auxCart[i].quantity = data.shopCart[i].quantity;
-                }
-            });
-        setShopCart(auxCart);
+    // fetches the id's and quantities of all products in the user's shopping cart
+    // and assigns these values to the shopCart array
+    // Each value of the shopCart array should be an object like this: {id:random_id, quantity:number_of_products }]
+    useEffect(async () => {
+        let auxCart = []; // auxiliary array that will receive the result of the fetch request
+        // postData: performs a POST request with {userId:userId} as the body of the request
+        const data = await postData(`https://electroshopping-user-regist.herokuapp.com/getShopCart/${userId}`, { userId: userId })
+        for (let i = 0; i < data.shopCart.length; i++) {
+            auxCart[i] = {};
+            auxCart[i].id = data.shopCart[i].id; //this shopCart is not the same shopCart state variable. Just a value received from the database
+            auxCart[i].quantity = data.shopCart[i].quantity;
+        }
+        let aux = [...auxCart]; // creates a copy of shopCart
+        for (let i = 0; i < auxCart.length; i++) {
+            // fetches the details of each product
+            const res = await fetch(`https://api-do-joao.herokuapp.com/find/id/${auxCart[i].id}`);
+            const resData = await res.json();
+            aux[i].image = resData[0].image;
+            aux[i].title = resData[0].title;
+            aux[i].price = resData[0].price;
+            aux[i].oldPrice = resData[0].oldPrice;
+            aux[i].type = resData[0].type;
+        }
+        setShopCart(aux);
     }, [])
 
-    useEffect(() => {
-
-    })
-
-    useEffect(() => {
-        console.log('useEffect2');
-        if (!isFirstRender) {
-            console.log('cart');
-            let aux = [...shopCart];
-            for (let i = 0; i < shopCart.length; i++) {
-                fetch(`https://api-do-joao.herokuapp.com/find/id/${shopCart[i].id}`).
-                    then(res => res.json()).
-                    then(res => {
-                        aux[i].image = res[0].image;
-                        aux[i].title = res[0].title;
-                        aux[i].price = res[0].price;
-                        aux[i].oldPrice = res[0].oldPrice;
-                        aux[i].type = res[0].type;
-                    });
+    return (
+        <div class={styles.container}>
+            <h1>Product</h1><h1>Quantity</h1><h1>Unit price</h1><h1>Total price</h1><div></div>
+            {
+                shopCart.map((product, i, array) => <CartItem key={product.id} product={product} index={i} array={array} />)
             }
-            setShopCart(aux);
-        }
-        console.log(shopCart);
-    }, [runFlag]);
-
-    useEffect(() => {
-        if (count <= 2000) {
-            setCount(prev => prev + 1);
-        }
-    }, [count])
-
-    if (count >= 2000) {
-        console.log(shopCart);
-        return (
-            <div class={styles.container}>
-                <h1>{shopCart[0] !== undefined ? Object.keys(shopCart).length : 'empty'}</h1>
-                <h1>Product</h1><h1>Quantity</h1><h1>Unit price</h1><h1>Total price</h1><div></div>
-                {
-                    shopCart.map((product, i, array) => <CartItem key={product.id} product={product} index={i} array={array} />)
-                }
-            </div>
-        )
-    } else {
-        return (
-            <div><h1>{count}</h1></div>
-        )
-    }
+        </div>
+    )
 }
 export default ShopCartPage
